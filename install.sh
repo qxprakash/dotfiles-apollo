5f355964af7e960b93b23be0e2b4a4e0e1550faf
Prakash <qxprakash@gmail.com>
2025-09-08 22:27:52 +0530

    Update regolith package key

--- install.sh
+++ install.sh
@@ -6,13 +6,12 @@ export DEBIAN_FRONTEND=noninteractive
 
 ## install regolith
 
-wget -qO - https://regolith-desktop.org/regolith.key | \
-gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
+# Register the Regolith public key to your local apt
+wget -qO - https://archive.regolith-desktop.com/regolith.key | gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
 
-echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] \
-https://regolith-desktop.org/release-ubuntu-jammy-amd64 jammy main" | \
+# Add the repository URL to your local apt
+echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] https://archive.regolith-desktop.com/ubuntu/stable jammy v3.3" | \
 sudo tee /etc/apt/sources.list.d/regolith.list
-
 
 sudo apt update -y
 sudo apt install -y regolith-desktop
