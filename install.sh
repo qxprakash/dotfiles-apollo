a7ddb30aa1def17378941924c5bba5efd11acaf3
Prakash <qxprakash@gmail.com>
2025-09-16 10:15:30 +0530

    fix picom build

--- install.sh
+++ install.sh
@@ -156,7 +156,7 @@ libxcb-shape0-dev libpcre3-dev libxcb-render-util0-dev libxcb-render0-dev \
 libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev \
 libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev \
 libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev \
-meson ninja-build
+meson ninja-build cmake libeproxy-dev libxcb-util-dev
 
 ## build & install picom fork
 echo ">>> Building custom Picom"
